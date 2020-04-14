b62dd96cd32eb71a211d73a9d75923a157eea15e
Danya0x07 <danya9104449383@gmail.com>
2020-04-14 20:15:09 +0300

    Unit-тесты для productionboard проходят.

--- lib/Buzzer/Buzzer.cpp
+++ lib/Buzzer/Buzzer.cpp
@@ -8,6 +8,7 @@ Buzzer::Buzzer(uint8_t pin, bool inverse) : pin(pin), inverted(inverse)
 
 void Buzzer::buzz(uint8_t times, uint8_t ms)
 {
+#ifndef UNIT_TEST
     for (uint8_t i = 0; i < times; i++)
     {
         digitalWrite(pin, 1);
@@ -15,4 +16,5 @@ void Buzzer::buzz(uint8_t times, uint8_t ms)
         digitalWrite(pin, 0);
         delay(ms / 2);
     }
+#endif
 }
